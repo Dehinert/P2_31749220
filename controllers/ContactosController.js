a5b4c9c66c628a4fae540cc9c5a264f408e9bec4
Dehinert <dehinertmoransjc@gmail.com>
2024-06-01 01:29:56 -0400

    controladores

--- controllers/ContactosController.js
+++ controllers/ContactosController.js
@@ -6,6 +6,31 @@ class ContactosController {
     this.add = this.add.bind(this);
   }
 
+  async obtenerIp() {
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      return data.ip; // Retorna la ip
+    } catch (error) {
+      console.error('Error al obtener la ip:', error);
+      return null; // Retorna null si hay un error
+    }
+  }
+
+  async obtenerPais(ip) {
+    try {
+      const response = await fetch('https://ipinfo.io/'+ip+'?token=197c576e48e0c3');
+      const data = await response.json();
+      return data.country; // Retorna el país
+    } catch (error) {
+      console.error('Error al obtener el país:', error);
+      return null; // Retorna null si hay un error
+    }
+  }
+
+
+
+
   async add(req, res) {
     // Validar los datos del formulario
 
@@ -17,11 +42,12 @@ class ContactosController {
     }
 
     // Guardar los datos del formulario
-    const ip = req.ip;
+    const ip = await this.obtenerIp();
     const fecha = new Date().toISOString();
+    const pais = await this.obtenerPais(ip)
 
    
-    await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha);
+    await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha, pais);
 
     const contactos = await this.contactosModel.obtenerAllContactos();
 
